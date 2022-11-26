5c7468fbb6582e309d0f4d9c958d3e5590219983
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-11-26 12:55:44 +0400

    js enter

---
+++ JavaScript/lesson2-operators/js/script.js
@@ -0,0 +1,60 @@
+
+//  Assingment Operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+
+//  Arithmetic Operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+// Comperison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <=
+// >
+// >=
+
+// var info = "1";
+// var data = 5;
+
+// if (info <=  data) {
+//         console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// Logical operators
+// &&
+// ||
+// !
+// if (false || !true || false || false || false) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// string operators
+// +
+// +=
+var a = "Hello";
+var b = "JavaScript";
+a+=b;
+console.log(a);

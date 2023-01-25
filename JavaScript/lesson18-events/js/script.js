d9dee1b5c89e059c527aa69197e9fa898234fa7c
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-25 12:42:20 +0400

    library

---
+++ JavaScript/lesson18-events/js/script.js
@@ -0,0 +1,69 @@
+// const input = document.querySelector('input');
+// input.onkeydown=()=>{
+//     input.style.backgroundColor = 'red';
+//     input.style.transition = '1s';
+//     input.style.height = '70px';
+// }
+
+// input.onkeyup=()=>{
+//     input.style.backgroundColor = 'white';
+//     input.style.transition = '1s';
+//     input.style.height = '30px';
+// }
+// input.onchange=(e)=>{
+//     document.querySelector('p').innerHTML = e.target.value;
+// }
+
+// const btn = document.querySelector("button");
+// btn.onmousedown = () => {
+//     btn.innerHTML = 'hey u press button'
+// };
+
+// btn.onmouseup = () => {
+//     btn.innerHTML = 'yes u very good human'
+// };
+
+// btn.onmouseover = () => {
+//     btn.innerHTML = 'hey u press button'
+// };
+
+// btn.onmouseout = () => {
+//     btn.innerHTML = 'yes u very good human'
+// };
+
+// btn.onmouseenter = () => {
+//     btn.innerHTML = 'hey u press button'
+// };
+
+// btn.onmouseleave = () => {
+//     btn.innerHTML = 'yes u very good human'
+// };
+
+
+
+
+// const h1 = document.querySelector('h1');
+// const text = document.querySelector('p');
+// const btn = document.querySelector('button');
+
+// btn.onclick =()=>{
+//     h1.innerHTML = 'Ders';
+//     text.innerHTML = 'Ders cox yaxsidir!';
+//     localStorage.setItem('lang','az');
+// }
+
+// if (localStorage.getItem('lang') === null) {
+//     localStorage.setItem('lang','en');
+// }else{
+//     switch (localStorage.getItem('lang')) {
+//         case 'en':
+//             h1.innerHTML = 'Lesson';
+//             text.innerHTML = 'Lesson is good';
+//             break;
+    
+//         default:
+//             h1.innerHTML = 'Ders';
+//             text.innerHTML = 'Ders cox yaxsidir!';
+//             break;
+//     }
+// }
